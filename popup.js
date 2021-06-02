3706ec480f7bd80143a7580da5b2007a6d8131e8
oelbaytam <oelbaytam@gmail.com>
2021-06-02 10:22:47 -0400

    initial upload

---
+++ popup.js
@@ -0,0 +1,14 @@
+
+document.getElementById('submit').addEventListener('click', Calculate);
+
+function Calculate() {
+    // Find values of input boxes.
+    var ICL = document.getElementById("ICL").value;
+    var DCL = document.getElementById("DCL").value;
+    
+    // Calculate Level of card.
+    for (var xp = 0;ICL <= DCL-1; ICL ++) {xp += +ICL;} xp *= 100;
+
+    // Return/output final answer.
+    document.getElementById('return').innerHTML = xp + "xp is required for level " + DCL;
+}
